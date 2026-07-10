4473659b9501197b5220d079ec2e846b76461207
Kahn <morderkaiserr83@gmail.com>
2026-07-10 09:04:54 +0900

    수심 100m

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          수심 100m
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=Klh683_rUmw"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          더 깊은 곳으로
         </a>
       </header>
     </div>
