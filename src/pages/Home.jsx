9da0ef882b43486befcb89be83d1b5e7e87bb608
Kahn <morderkaiserr83@gmail.com>
2026-07-26 19:25:38 +0900

    수심 500m 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,81 @@
-
-import '../styles/Home.css';
-import { useNavigate } from 'react-router-dom'
+import "../styles/Home.css";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Home() {
     const navigate = useNavigate();
+    const [depth, setDepth] = useState("100");
+    const [moving, setMoving] = useState(false);
+    const [arrived, setArrived] = useState(false);
+
+    const goDeeper = () => {
+        if (moving) return;
+
+        setMoving(true);
+
+        setTimeout(() => {
+
+            setDepth("500");
+            setArrived(true);
+            /*    setMoving(false); */
+
+        }, 2000);
+
+    };
+
+
+
     return (
+
         <div className="App">
+
+
             <button
                 className="guest-book-button"
                 onClick={() => navigate("/guestbook")}
             >
                 방명록
             </button>
-            <header className="App-header">
-                <p>
-                    수심 100m
-                </p>
-                <a
-                    className="App-link"
-                    href="https://www.youtube.com/watch?v=Klh683_rUmw"
-                    target="_blank"
-                    rel="noopener noreferrer"
+
+            {depth === "100" && (
+                <div
+                    className={`screen depth100 ${moving ? "move-up" : ""
+                        }`}
                 >
-                    더 깊은 곳으로
-                </a>
-            </header>
+
+                    <div className="content">
+                        <p>수심 100m</p>
+                        <button
+                            className="App-link"
+                            onClick={goDeeper}
+                        >
+                            더 깊은 곳으로
+                        </button>
+                    </div>
+                </div>
+            )}
+
+            {(depth === "100" || depth === "500") && (
+                <div
+                    className={`screen depth500 ${moving || arrived ? "move-in" : ""
+                        }`}
+                >
+                    <div className="content">
+                        <p>수심 500m</p>
+                        {(depth === "500" || moving) && (
+                            <button
+                                className="App-link"
+                            >
+                                아주 깊은 곳으로
+                            </button>
+                        )}
+                    </div>
+                </div>
+            )}
         </div>
     );
+
 }
+
 
 export default Home;
