7f7ea7541c492059ba0a12cdf5d2abf70e990487
Kahn <morderkaiserr83@gmail.com>
2026-09-04 17:58:29 +0900

    네레아 비석 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ function Home({ playing, toggleMusic }) {
     const [state, setState] = useState("");
     const [reason, setReason] = useState("");
     const [need, setNeed] = useState("");
-    const [letter, setLetter] = useState("");
+    /* const [letter, setLetter] = useState(""); */
 
     useEffect(() => {
 
