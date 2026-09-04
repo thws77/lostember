cee8da3f59970bea0d703dddb9198c4c777384af
Kahn <morderkaiserr83@gmail.com>
2026-09-04 17:35:06 +0900

    네레아 페이지 추가 및 비석 기본 UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,14 @@ function Home({ playing, toggleMusic }) {
 
     const appRef = useRef(null);
     const [fading, setFading] = useState(false);
+    const [showPopup, setShowPopup] = useState(false);
+    const [showPopup2, setShowPopup2] = useState(false);
+
+    const [name, setName] = useState("");
+    const [state, setState] = useState("");
+    const [reason, setReason] = useState("");
+    const [need, setNeed] = useState("");
+    const [letter, setLetter] = useState("");
 
     useEffect(() => {
 
@@ -65,6 +73,8 @@ function Home({ playing, toggleMusic }) {
     const depth1000 = useRef(null);
     const depth5000 = useRef(null);
     const depthBottom = useRef(null);
+    const depthNerea = useRef(null);
+
 
     const moveTo = async (target, duration) => {
 
@@ -99,6 +109,10 @@ function Home({ playing, toggleMusic }) {
 
     const goBottom = () => {
         moveTo(depthBottom.current, 1700);
+    };
+
+    const goNerea = () => {
+        moveTo(depthNerea.current, 2000);
     };
 
     return (
@@ -155,6 +169,12 @@ function Home({ playing, toggleMusic }) {
                         onClick={go500}
                     >
                         더 깊은 곳으로
+                    </button>
+                    <button
+                        className="App-link"
+                        onClick={goNerea}
+                    >
+                        단숨에 바닥까지
                     </button>
                 </div>
             </section>
@@ -232,8 +252,137 @@ function Home({ playing, toggleMusic }) {
                     >
                         다시 수면 위로
                     </button>
+                    <button
+                        className="App-link"
+                        onClick={() => {
+                            if (window.confirm("내려갈까요? 다시 올라올 수 없습니다.")) {
+                                goNerea();
+                            }
+                        }}>
+                        잠깐... 뭔가 있는 것 같은데.
+                    </button>
                 </div>
             </section>
+            <section
+                ref={depthNerea}
+                className="screen depth-Nerea">
+                <div className="content">
+                    <p style={{
+                        marginBottom: "1px",
+                        marginTop: "1px"
+                    }}>Nerea</p>
+                    <p style={{
+                        marginTop: "1px",
+                        fontSize: "calc(20px + 2vmin)"
+                    }}>잊혀진 마을</p>
+                    <button className="check-button"
+                        onClick={() => setShowPopup(true)}>
+                        조사하기
+                    </button>
+                </div>
+
+                {showPopup && (
+                    <div className="popup">
+                        <div className="popup-box">
+                            <h2>신비로운 비석</h2>
+                            <p>어딘가 낡았지만 정교하게 조각된 비석. 따뜻한 기운이 흘러나온다.</p>
+                            <button
+                                className="X-button"
+                                onClick={() => setShowPopup(false)}>
+                                X
+                            </button>
+                            <button className="App-link"
+                                onClick={() => {
+                                    setShowPopup(false);
+                                    setShowPopup2(true);
+                                }}>
+                                비석에 무언가를 새겨볼까?
+                            </button>
+                        </div>
+                    </div>
+                )}
+
+                {showPopup2 && (
+                    <div className="popup">
+                        <div className="popup-box2">
+                            <h2>신비로운 비석</h2>
+                            <button
+                                className="X-button"
+                                onClick={() => setShowPopup2(false)}>
+                                X
+                            </button>
+                            <p>당신의 이름을 알려주세요.</p>
+                            <input
+                                className="name-input"
+                                type="text"
+                                placeholder="ex)고길동"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <p>당신이 여기까지 온 이유는 무엇인가요?</p>
+                            <input
+                                className="name-input"
+                                type="text"
+                                placeholder="ex)자유를 찾아서"
+                                value={reason}
+                                onChange={(e) => setReason(e.target.value)}
+                            />
+
+                            <p>여기까지 온 당신은...</p>
+
+                            <button
+                                className={state === "멀쩡" ? "choice-button-on" : "choice-button"}
+                                onClick={() => setState("멀쩡")}>
+                                멀쩡해요!
+                            </button>
+
+                            <button
+                                className={state === "지침" ? "choice-button-on" : "choice-button"}
+                                onClick={() => setState("지침")}>
+                                조금 지쳤어요.
+                            </button>
+
+                            <button
+                                className={state === "힘듦" ? "choice-button-on" : "choice-button"}
+                                onClick={() => setState("힘듦")}>
+                                많이 힘들어요.
+                            </button>
+
+                            <button
+                                className={state === "만신창이" ? "choice-button-on" : "choice-button"}
+                                onClick={() => setState("만신창이")}>
+                                만신창이가 되었어요...
+                            </button>
+
+                            <p>지금 당신에게는 어떤 것이 필요한가요?</p>
+
+                            <button
+                                className={need === "위로" ? "choice-button-on" : "choice-button"}
+                                onClick={() => setNeed("위로")}>
+                                위로를 받고 싶어요.
+                            </button>
+
+                            <button
+                                className={need === "용기" ? "choice-button-on" : "choice-button"}
+                                onClick={() => setNeed("용기")}>
+                                용기를 내고 싶어요.
+                            </button>
+
+                            <button
+                                className={need === "휴식" ? "choice-button-on" : "choice-button"}
+                                onClick={() => setNeed("휴식")}>
+                                휴식이 필요해요.
+                            </button>
+
+                            <button
+                                className="letter-button"
+                                onClick={() => setLetter(true)}>
+                                비석에 충분히 글을 새겼다.
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </section >
         </div >
     );
 }
